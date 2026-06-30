98b13f05261fd43fcb7ec36f9ac612706395a4d8
AdrianHordyk <ar.hordyk@gmail.com>
2026-06-30 12:23:09 -0700

    minor patch
    fix violin plot for multiple OMs

--- R/fct_plotBoxplot.R
+++ R/fct_plotBoxplot.R
@@ -121,16 +121,20 @@ plotBoxplot <- function(slick, PI=NULL, type=c('boxplot', 'violin', 'both', 'all
 
 
   if (!byOM) {
-    box_df <- df |> dplyr::group_by(Sim, MP, PI) |>
-      dplyr::mutate(Mean=mean(value, na.rm=TRUE)) |>
+    df <- df |> dplyr::group_by(Sim, MP, PI) |>
+      dplyr::mutate(value=mean(value, na.rm=TRUE))
+
+    box_df <- df |>
       dplyr::group_by(MP, PI) |>
-      dplyr::summarize(m=median(Mean, na.rm=TRUE),
-                       low1=quantile(Mean, 0.25, na.rm=TRUE),
-                       upp1=quantile(Mean, 0.75, na.rm=TRUE),
-                       low2=min(Mean, na.rm=TRUE),
-                       upp2=max(Mean, na.rm=TRUE))
+      dplyr::summarize(m=median(value, na.rm=TRUE),
+                       low1=quantile(value, 0.25, na.rm=TRUE),
+                       upp1=quantile(value, 0.75, na.rm=TRUE),
+                       low2=min(value, na.rm=TRUE),
+                       upp2=max(value, na.rm=TRUE))
     box_df$MP <- factor(box_df$MP, ordered=TRUE, levels=MP_lab)
     box_df$PI <- factor(box_df$PI, ordered=TRUE, levels=PI_names)
+
+
   } else {
     box_df <- df |>
       dplyr::group_by(OM, MP, PI) |>
@@ -183,7 +187,6 @@ plotBoxplot <- function(slick, PI=NULL, type=c('boxplot', 'violin', 'both', 'all
     ggplot2::geom_pointrange(data = box_df,
                              ggplot2::aes(x=MP, y=m, ymin=low1, ymax=upp1, color=MP, fill=MP),
                              linewidth = 2, shape = 21, inherit.aes = FALSE, size=1.5)
-
 
 
   p2 <- p + ggplot2::geom_violin(scale='width', ggplot2::aes(y=value))
